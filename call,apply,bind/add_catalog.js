c3b35a840287e1edd4cffcdb7b6f35094e512341
Denis, Makarov <makarovdenis07081985@gmail.com>
2020-04-28 12:54:28 +0300

    add_catalog

---
+++ call,apply,bind/add_catalog.js
@@ -0,0 +1,38 @@
+//// ADD TO CATALOG
+// function valid() {
+//
+//     let name = document.getElementById("name");
+//     let price = document.getElementById("price");
+//     let description = document.getElementById("description");
+//     let gander = document.getElementById("gander");
+//     let img = document.getElementById("img");
+//
+//
+//     let prodModel = {
+//         "name": name.value,
+//         "price": price.value,
+//         "data": 1,
+//         "sex": gander.value,
+//         "data-check": 88,
+//         "image": img.value,
+//         "description": description.value,
+//         "more_description":  description.value
+//     };
+//     // console.log(addProd);
+//     products.push(prodModel)
+//     alert("Item was add in catalog!");
+//     document.getElementById("addItem_box").style.height = "0px";
+// }
+// function showProdAddBox() {
+//     add_items.addEventListener("click", (e) => {
+//         let x = document.getElementById("addItem_box");
+//         x.style.height = "420px";
+//         console.log(x);
+//     });
+//     document.body.addEventListener("keydown", (e) => {
+//         let x = document.getElementById("addItem_box");
+//         if (e.keyCode == 27) {
+//             x.style.height = "0px";
+//         };
+//     });
+// }
