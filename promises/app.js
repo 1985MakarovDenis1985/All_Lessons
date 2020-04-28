9296ceb8a17314eb9ada3ec4c34e809b43bc6916
Denis, Makarov <makarovdenis07081985@gmail.com>
2020-04-28 13:00:58 +0300

    promises

---
+++ promises/app.js
@@ -0,0 +1,48 @@
+
+// let a = 7;
+// console.log(a);
+//
+// let b = new Promise(function (resolve, reject) {
+//     setTimeout(function () {
+//       resolve(a += 8);
+//     },1000)
+// })
+// b.then(function () {
+//     console.log(a)
+// })
+
+
+
+
+
+
+// ====================== reduce ======================= //
+
+// let x = [1,2,3,4,5,6,7,8,9];
+// let y = x.reduce(function (total, number) {
+//     return total + number
+// });
+// console.log(y);
+
+
+// ========================== every ==================== //
+// => проверяет является ли каждый елимент массива единому значению и выдает либо false либо true
+
+// let x = [1,2,3,4,5,6,7,8,9];
+// let y = x.every(function (value, index, array) {
+//     return value > 5
+// });
+// console.log(y);
+
+
+// ========================== some ==================== //
+// => проверяет является ли хоть один елимент массива единому значению и выдает либо false либо true
+
+// let x = [1,2,3,4,5,6,7,8,9];
+// let y = x.some(function (value, index, array) {
+//     return value > 5
+// });
+// console.log(y);
+
+
+
