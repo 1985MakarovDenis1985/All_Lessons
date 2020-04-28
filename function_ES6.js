bca0334d9945eade5f27ef11b035f2267a4a6a64
Denis, Makarov <makarovdenis07081985@gmail.com>
2020-04-28 12:58:21 +0300

    fun ES6 part one

---
+++ function_ES6.js
@@ -0,0 +1,53 @@
+'use strict';
+
+
+//=================== стрелочные функции ES6
+//
+// var x = 5;
+// var y = 3;
+// var c;
+// c = function add() {
+//   return x + y;
+// };
+// console.log(c());
+
+//====================
+
+// var x = 5;
+// var y = 3;
+// var c;
+// c = (a,b) => a+b;
+// console.log(c(x,y));
+
+//====================  //
+//
+// var x = 5;//                   // если больше 2 строк
+// var y = 3;//                // берем условие в скобки {}
+// var c = (a,b) => { //       // и возвращаем через return
+//   var ccc = a*b;
+//   return ccc;
+// };
+// console.log(c(x,y));
+
+//// =========================
+
+// var c = function() {          // var c = () => ({"name": "John"})
+//   return {"name": "John"}     // литерал обьекта закрываем в () скобки
+// };                            //
+// console.log(c());             //
+// console.log(typeof c());      //
+//
+// var c = () => ({"name": "John"})
+// console.log(c());
+
+//===================
+
+// var egg = () => console.log("стрелочная функция"); // еще сокращение
+// egg();
+
+
+
+
+
+
+//
