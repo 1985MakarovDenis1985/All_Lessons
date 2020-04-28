c4966a67fdb9e6984436773d2eac4be7dd733b5f
Denis, Makarov <makarovdenis07081985@gmail.com>
2020-04-28 12:55:29 +0300

    dom

---
+++ call,apply,bind/DOM.js
@@ -0,0 +1,22 @@
+//                           Работа с DOM
+
+// var aArr = ["home", "about us", "contacts", "CV", "preview"]
+
+// var nav = document.getElementById("navigation"); // получаем доступ к ID
+// console.log(nav);
+//
+// var ul = document.createElement("ul"); //          создаем елемент ul
+// ul.innerHTML = "main_ul";              //          наполняем его контентом
+// ul.setAttribute("class","main_ul"); //            создаем класс
+//nav.appendChild(ul);                   //          добовляем новый елимент ul в nav
+//
+// for (var i = 0; i < 5; i++) {
+//   var li = document.createElement("li"); //        создаем елемент li
+//   ul.appendChild(li);                    //        добовляем новый елимент li в ul
+//   for (var j = 0; j < 3; j++) {
+//     var a = document.createElement("a"); //        создаем елемент a
+//     a.setAttribute("href","nav-link"); //          создаем ссылку
+//     a.innerHTML = "aArr[j]";             //        наполняем его контентом
+//     li.appendChild(a);                   //        добовляем новый елимент ul в nav
+//   }
+// }
