79975c8a18e90fcaa78a726cd9a4e5552194dfba
Denis, Makarov <makarovdenis07081985@gmail.com>
2020-05-02 20:48:38 +0300

    promise || .then, .catch, .finaly || .all, .race

--- promises/app.js
+++ promises/app.js
@@ -11,6 +11,88 @@
 //     console.log(a)
 // })
 
+// =====================  instance #2 ========================= //
+
+// let obj = {};
+// let p = new Promise((resolve, reject) => {
+//     console.log("preparing ...");
+//     setTimeout(() => {
+//         const backEndData = {
+//             server: "aws",
+//             port: "3030",
+//             status: "working",
+//         };
+//         obj = {
+//             server: "Tiphon",
+//             port: 9000,
+//         };
+//         resolve([backEndData, obj]) // => для передачи неск. ел => []
+//     }, 1000)
+// })
+//     .then(([backEndData, obj]) => { // => для приема неск. ел => []
+//         return new Promise((resolve, reject) => {
+//             setTimeout(() => {
+//                 console.log("------------------ 1");
+//                 console.log(backEndData);
+//                 console.log(obj);
+//                 resolve([backEndData, obj]);
+//             }, 1000);
+//         })
+//     })
+//     .then(([backEndData, obj]) => {
+//         return new Promise((resolve, reject) => {
+//             setTimeout(() => {
+//                 obj.port = 8000;
+//                 console.log("------------------ 2");
+//                 console.log(backEndData)
+//                 console.log(obj)
+//                 resolve(backEndData)
+//             }, 500)
+//         })
+//     })
+//     .catch((err) => {  //-------------- перехватываем ошибки
+//         console.error("ошибка", err)
+//     })
+//     .finally(()=>{ // --------- финальные действия в итоге
+//         console.log("Finaly")
+//     })
+//
+//
+// // ------------------------ ФУНКЦИИ ЧЕРЕЗ ПРОМИСЫ | .ALL  .RACE -----------------------
+//
+// const sleep = function (ms) {
+//     return new Promise((res, rej) =>{
+//         setTimeout(()=>{
+//             res()
+//         }, ms)
+//     })
+// };
+//
+// sleep(2000).then(() => {console.log("еще рано")});
+// sleep(3000).then(() => {console.log("пора вставать")});
+//
+// /*
+// .all =>  then выполняется после всех выполненых промисов
+//  */
+// Promise.all([
+//     sleep(3000).then(()=>console.log("3000")),
+//     sleep(2000).then(()=>console.log("2000"))
+// ])
+//     .then(()=>{ // => после выполнения всех промисов выполняется then
+//         console.log("completed All")
+//     });
+//
+// /*
+// .race =>  then выполняется после первого выполненного промиса
+//  */
+// Promise.race([
+//     sleep(3000).then(()=>console.log("3000")),
+//     sleep(2000).then(()=>console.log("2000"))
+// ])
+//     .then(()=>{
+//         console.log("completed Race")
+//     })
+
 
 
 
